19d82833a5a8bf97fd818a8a1907d6e707a4f4a5
santafg <santanughosh28111998@gmail.com>
2022-12-22 02:05:53 +0530

    variables , union , type , function

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+// // Variables
+
+// let name: string;
+// let name: unknown;
+// let age: number;
+// let isStudent: boolean;
+// let hobbies: string[];
+
+// // union
+
+// let age: string | number;
+
+// let role: [number, string];
+
+// type Person = {
+//   name: string;
+//   age?: number;
+// };
+
+// let person: Person = {
+//   name: "Santanu",
+//   age: 10,
+// };
+// let person2: Person = {
+//   name: "Santanu",
+// };
+
+// let lotsOfPersons: Person[];
+
+// // function
+
+// function printName(name: string) {
+//   console.log(name);
+// }
+
+// printName("Santanu");
+
+// let printName : Function ;
+
+// let printName: (name: string) => void ;
+// let printName: (name: string) => never ;
+let printName: (name: string) => string ;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>hwllo</div>;
 }
 
 export default App;
